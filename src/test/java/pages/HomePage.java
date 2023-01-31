2e56a670fb661c3d1257ae19bf9d1610c11df2e1
Andjela Colic <colicandjela98@gmail.com>
2023-01-31 20:48:14 +0100

    Tests for login and logout added

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -10,11 +10,18 @@ public class HomePage extends BasePage{
     @FindBy(xpath = "//*[@id=\"app\"]/div/div/header/div/div[3]/a[3]")
     private WebElement login;
 
+    @FindBy(xpath = "//*[@id=\"app\"]/div/div/header/div/div[3]/button[2]")
+    private WebElement logout;
+
     public HomePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
 
     public void openLogin(){
         login.click();
+    }
+
+    public void logout(){
+        logout.click();
     }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,17 @@
 package tests;
 
 import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
+
+import java.time.Duration;
 
 public class LoginTests extends BaseTest{
     private HomePage homePage;
@@ -39,6 +44,35 @@ public class LoginTests extends BaseTest{
         homePage.openLogin();
         loginPage.login(faker.internet().emailAddress(), faker.internet().password());
         Assert.assertEquals(loginPage.messageToString(), "User does not exists");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
+
+    @Test
+    public void wrongPasswordLogin() {
+        Faker faker = new Faker();
+        homePage.openLogin();
+        loginPage.login("admin@admin.com", faker.internet().password());
+        Assert.assertEquals(loginPage.messageToString(), "Wrong password");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
+
+    @Test
+    public void login() {
+        homePage.openLogin();
+        loginPage.login("admin@admin.com", "12345");
+        driverWait.until(ExpectedConditions.urlContains("/home"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("/home"));
+    }
+
+    @Test
+    public void logout() {
+        homePage.openLogin();
+        loginPage.login("admin@admin.com", "12345");
+        driverWait.until(ExpectedConditions.urlContains("/home"));
+        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/header/div/div[3]/button[2]")).isDisplayed());
+        homePage.logout();
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+        driver.get("https://vue-demo.daniel-avellaneda.com/home");
         Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
     }
 }
