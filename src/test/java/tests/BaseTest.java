5c3a4d6e92ae7e0cd47649446a438a7cf6302305
Andjela Colic <colicandjela98@gmail.com>
2023-01-31 18:34:45 +0100

    Initial commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait driverWait;
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\colic\\OneDrive\\Radna površina\\QA\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get("https://vue-demo.daniel-avellaneda.com");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void afterMehtod() {
+        driver.quit();
+    }
+}
