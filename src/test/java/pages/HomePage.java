1965fea43a5c5cc3c58a9331cbc136870a5bcc20
Andjela Colic <colicandjela98@gmail.com>
2023-02-06 15:06:59 +0100

    Locale tests (ES, EN, FR) - added

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,8 +1,11 @@
 package pages;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage{
@@ -15,6 +18,21 @@ public class HomePage extends BasePage{
 
     @FindBy(xpath = "//*[@id=\"app\"]/div/div/header/div/div[3]/a[4]")
     private WebElement singupBtn;
+
+    @FindBy(className = "btnLocaleActivation")
+    private WebElement localeBtn;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")
+    private WebElement header;
+
+    @FindBy(xpath = "//*[@id=\"list-item-75\"]")
+    private WebElement localeES;
+
+    @FindBy(xpath = "//*[@id=\"list-item-73\"]")
+    private WebElement localeEN;
+
+    @FindBy(xpath = "//*[@id=\"list-item-77\"]")
+    private WebElement localeFR;
 
     public HomePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
@@ -34,5 +52,28 @@ public class HomePage extends BasePage{
 
     public WebElement logoutBtn() {
         return logout;
+    }
+
+    public void localeBtn(){
+        localeBtn.click();
+    }
+
+    public String header(){
+       return header.getText();
+    }
+
+    public void localeES (){
+        driverWait.until(ExpectedConditions.visibilityOf(localeES));
+        localeES.click();
+    }
+
+    public void localeEN(){
+        driverWait.until(ExpectedConditions.visibilityOf(localeEN));
+        localeEN.click();
+    }
+
+    public void localeFR(){
+        driverWait.until(ExpectedConditions.visibilityOf(localeFR));
+        localeFR.click();
     }
 }
--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -1,4 +1,51 @@
 package tests;
 
-public class LocaleTests {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.AdminCitiesPage;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.util.HashMap;
+
+public class LocaleTests extends BaseTest{
+
+    private HomePage homePage;
+
+    @BeforeClass
+    @Override
+    public void beforeClass() {
+        super.beforeClass();
+        homePage = new HomePage(driver, driverWait);
+    }
+
+    @BeforeMethod
+    @Override
+    public void beforeMethod() {
+        super.beforeMethod();
+        homePage.localeBtn();
+    }
+
+    @Test
+    public void localeES(){
+        homePage.localeES();
+        Assert.assertTrue(homePage.header().contains("Página de aterrizaje"));
+    }
+
+    @Test
+    public void localeEN(){
+        homePage.localeEN();
+        Assert.assertTrue(homePage.header().contains("Landing"));
+    }
+
+    @Test
+    public void localeFR(){
+        homePage.localeFR();
+        Assert.assertTrue(homePage.header().contains("Page d'atterrissage"));
+    }
 }
