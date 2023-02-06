57e2500b48c86074b397b20f2c4efa90ebc18932
Andjela Colic <colicandjela98@gmail.com>
2023-02-06 14:34:16 +0100

    AuthRoutes tests added

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -1,4 +1,31 @@
 package tests;
 
-public class AuthRoutesTests {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AuthRoutesTests extends BaseTest {
+
+    @Test
+    public void visitToHome() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/home");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
+
+    @Test
+    public void profilePage() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/profile");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
+
+    @Test
+    public void adminCitiesPage() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
+
+    @Test
+    public void adminUserPage() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/users");
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"));
+    }
 }
